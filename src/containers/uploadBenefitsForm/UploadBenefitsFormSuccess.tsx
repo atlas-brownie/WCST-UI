bfd50667c4a20c6fb28f9aa703150aa1adb60bdd
fultongr <greg.fulton@saic.com>
2020-07-08 07:28:20 -0400

    Run depcheck; remove unused packages; add packages back that are needed for build to run

--- src/containers/uploadBenefitsForm/UploadBenefitsFormSuccess.tsx
+++ src/containers/uploadBenefitsForm/UploadBenefitsFormSuccess.tsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 import { Link } from 'react-router-dom';
-import { IRootState, IUploadBenefitsResponsePayload } from 'src/types';
+import { IRootState, IUploadBenefitsResponsePayload } from '../../../src/types';
 
 const mapStateToProps = (state: IRootState) => {
   return {
