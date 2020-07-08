0153006e8b4a6c9135e4d1266d5df9a85417534f
fultongr <greg.fulton@saic.com>
2020-07-08 12:52:23 -0400

    Remove unneeded webpack plugin and make the tests that we do have pass.

--- src/actions/upload-benefits.spec.ts
+++ src/actions/upload-benefits.spec.ts
@@ -1,5 +1,33 @@
 import 'jest';
 
+import * as constants from '../types/constants';
+// import * as validators from '../utils/validators';
+import * as actions from './upload-benefits';
+
 afterEach(() => {
   fetchMock.resetMocks();
+});
+
+// const appState = {
+//   uploadBenefits: {
+//     inputs: {
+//       fileNumber: '321-22-3432',
+//       veteranFirstName: 'TestFirstName',
+//       veteranLastName: 'TestLastName',
+//       zipCode: '22003',
+//     },
+//   },
+// };
+
+describe('updateBenefitsFileNumber', () => {
+  it('should return the input value if the value is not dirty', () => {
+    const newValue = {
+      dirty: false,
+      value: '321-22-3432',
+    };
+    expect(actions.updateBenefitsFileNumber(newValue)).toEqual({
+      newValue,
+      type: constants.UPDATE_BENEFITS_FILE_NUMBER,
+    });
+  });
 });
