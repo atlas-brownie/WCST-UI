ce972f1812e164956cb8f53c2dec9aca6718f0b8
fultongr <greg.fulton@saic.com>
2020-07-09 22:25:53 -0400

    One more test

--- src/toHtmlId.spec.ts
+++ src/toHtmlId.spec.ts
@@ -6,4 +6,8 @@ describe('toHtmlId', () => {
   it('returns "toHtmlId"', () => {
     expect(toHtmlId);
   });
+
+  it('removes leading digit', () => {
+    expect(toHtmlId.toHtmlId('1abc')).toEqual('abc');
+  });
 });
